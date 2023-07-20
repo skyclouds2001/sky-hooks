f5d57d756f62c544a10323f532f5a037ec851261
c50034865 <chensiyuan49@huawei.com>
2023-07-20 17:47:42 +0800

    test: add useFavicon test

--- test/useFavicon.test.ts
+++ test/useFavicon.test.ts
@@ -1,8 +1,27 @@
 import { describe, expect, it } from 'vitest'
 import { useFavicon } from '../src'
+import { nextTick } from 'vue'
 
 describe('useFavicon', () => {
   it('should be defined', () => {
     expect(useFavicon).toBeDefined()
+  })
+
+  it('should initial to be null without initial value', async () => {
+    const favicon = useFavicon()
+    expect(favicon.value).toBeNull()
+
+    favicon.value = 'v1'
+    await nextTick()
+    expect(favicon.value).toBe('v1')
+  })
+
+  it('should initial to be initial value with initial value', async () => {
+    const favicon = useFavicon('v1')
+    expect(favicon.value).toBe('v1')
+
+    favicon.value = 'v2'
+    await nextTick()
+    expect(favicon.value).toBe('v2')
   })
 })
