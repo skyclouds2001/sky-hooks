c5f9c2c76561f4728dfd26e29ab2884af0dc2a24
skyclouds2001 <skyclouds2001@163.com>
2023-08-02 18:03:35 +0800

    feat: add unsupported error

--- src/util.ts
+++ src/util.ts
@@ -1,3 +1,13 @@
 export type Fn = (...args: any[]) => any
 
 export type Obj = Record<string, any>
+
+export class UnSupportedError extends Error {
+  constructor(type: string, message?: string, options?: ErrorOptions) {
+    super(message, options)
+
+    this.type = type
+  }
+
+  public readonly type: string
+}
