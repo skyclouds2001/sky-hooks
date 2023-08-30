39d348b75d3fb988a784ccca2b958b616af28345
skyclouds2001 <95597335+skyclouds2001@users.noreply.github.com>
2023-08-30 16:51:40 +0800

    Create useMedia.ts

---
+++ src/useMedia.ts
@@ -0,0 +1,126 @@
+import { type MaybeRefOrGetter, ref, watch, toValue } from 'vue'
+import useEventListener from './useEventListener'
+
+enum MediaType {
+  UNKNOWN = 'unknown',
+  AUDIO = 'audio',
+  VIDEO = 'video',
+}
+
+// todo https://github.com/vueuse/vueuse/blob/main/packages/core/useMediaControls/index.ts
+const useMedia = (target: MaybeRefOrGetter<HTMLMediaElement | null | undefined>): any => {
+  const element = toValue(target)
+
+  const mediaType = ref(MediaType.UNKNOWN)
+
+  // todo
+  const readyState = ref<HTMLMediaElement['HAVE_NOTHING' | 'HAVE_METADATA' | 'HAVE_CURRENT_DATA' | 'HAVE_FUTURE_DATA' | 'HAVE_ENOUGH_DATA']>(HTMLMediaElement.HAVE_NOTHING)
+
+  // todo
+  const networkState = ref<HTMLMediaElement['NETWORK_EMPTY' | 'NETWORK_IDLE' | 'NETWORK_LOADING' | 'NETWORK_NO_SOURCE']>(HTMLMediaElement.NETWORK_EMPTY)
+
+  const playing = ref(false)
+
+  const pausing = ref(true)
+
+  const play = async (): Promise<void> => {
+    return toValue(target)?.play()
+  }
+
+  const pause = async (): Promise<void> => {
+    return toValue(target)?.pause()
+  }
+
+  watch(
+    () => toValue(target),
+    (target) => {
+      if (target == null) {
+        mediaType.value = MediaType.UNKNOWN
+        readyState.value = HTMLMediaElement.HAVE_NOTHING
+        networkState.value = HTMLMediaElement.NETWORK_EMPTY
+        playing.value = false
+        pausing.value = true
+        return
+      }
+
+      if (target instanceof HTMLAudioElement) {
+        mediaType.value = MediaType.AUDIO
+      } else if (target instanceof HTMLVideoElement) {
+        mediaType.value = MediaType.VIDEO
+      } else {
+        mediaType.value = MediaType.UNKNOWN
+      }
+
+      if (playing.value) {
+        target.play()
+      }
+      if (pausing.value) {
+        target.pause()
+      }
+
+      useEventListener(
+        target,
+        'play',
+        () => {
+          playing.value = !target.paused
+          pausing.value = target.paused
+        },
+        {
+          passive: true,
+        },
+      )
+
+      useEventListener(
+        target,
+        'pause',
+        () => {
+          playing.value = !target.paused
+          pausing.value = target.paused
+        },
+        {
+          passive: true,
+        },
+      )
+
+      useEventListener(
+        target,
+        'playing',
+        () => {
+          playing.value = !target.paused
+          pausing.value = target.paused
+        },
+        {
+          passive: true,
+        },
+      )
+
+      useEventListener(
+        target,
+        'waiting',
+        () => {
+          playing.value = !target.paused
+          pausing.value = target.paused
+        },
+        {
+          passive: true,
+        },
+      )
+    },
+    {
+      immediate: true,
+    }
+  )
+
+  return {
+    element,
+    mediaType,
+    readyState,
+    networkState,
+    playing,
+    pausing,
+    play,
+    pause,
+  }
+}
+
+export default useMedia
