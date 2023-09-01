6942c3a105e6990ee4a3d1c358bae4a4b637d298
skyclouds2001 <95597335+skyclouds2001@users.noreply.github.com>
2023-09-01 15:04:14 +0800

    Update useEventListener.ts

--- src/useEventListener.ts
+++ src/useEventListener.ts
@@ -1,18 +1,30 @@
 import { getCurrentScope, onScopeDispose } from 'vue'
 
+type MaybeArray<T> = T | T[]
+
+type Listener<T extends EventTarget, E extends Event> = (this: T, e: E) => void
+
 type UseEventListenerOptions = AddEventListenerOptions | EventListenerOptions | boolean
 
-function useEventListener<E extends keyof WindowEventMap>(target: Window, event: E | E[], listener: (this: Window, e: WindowEventMap[E]) => void, options?: UseEventListenerOptions): () => void
+function useEventListener<E extends keyof WindowEventMap>(target: Window, event: MaybeArray<E>, listener: Listener<Window, WindowEventMap[E]>, options?: UseEventListenerOptions): () => void
 
-function useEventListener<E extends keyof DocumentEventMap>(target: Document, event: E | E[], listener: (this: Document, e: DocumentEventMap[E]) => void, options?: UseEventListenerOptions): () => void
+function useEventListener<E extends keyof DocumentEventMap>(target: Document, event: MaybeArray<E>, listener: Listener<Document, DocumentEventMap[E]>, options?: UseEventListenerOptions): () => void
 
-function useEventListener<E extends keyof ShadowRootEventMap>(target: ShadowRoot, event: E | E[], listener: (this: ShadowRoot, e: ShadowRootEventMap[E]) => void, options?: UseEventListenerOptions): () => void
+function useEventListener<E extends keyof ShadowRootEventMap>(target: ShadowRoot, event: MaybeArray<E>, listener: Listener<ShadowRoot, ShadowRootEventMap[E]>, options?: UseEventListenerOptions): () => void
 
-function useEventListener<E extends string>(target: EventTarget, event: E | E[], listener: (this: EventTarget, e: Event) => void, options?: UseEventListenerOptions): () => void
+function useEventListener<E extends keyof ElementEventMap>(target: Element, event: MaybeArray<E>, listener: Listener<Element, ElementEventMap[E]>, options?: UseEventListenerOptions): () => void
 
-function useEventListener<T extends EventTarget, M extends Record<string, any>, E extends keyof M>(target: T, event: E | E[], listener: (this: T, e: M[E]) => void, options?: UseEventListenerOptions): () => void
+function useEventListener<E extends keyof HTMLElementEventMap>(target: HTMLElement, event: MaybeArray<E>, listener: Listener<HTMLElement, HTMLElementEventMap[E]>, options?: UseEventListenerOptions): () => void
 
-function useEventListener(target: EventTarget, event: string | string[], listener: (this: EventTarget, e: Event) => void, options?: UseEventListenerOptions): () => void {
+function useEventListener<E extends keyof SVGElementEventMap>(target: SVGElement, event: MaybeArray<E>, listener: Listener<SVGElement, SVGElementEventMap[E]>, options?: UseEventListenerOptions): () => void
+
+function useEventListener<E extends keyof MathMLElementEventMap>(target: MathMLElement, event: MaybeArray<E>, listener: Listener<MathMLElement, MathMLElementEventMap[E]>, options?: UseEventListenerOptions): () => void
+
+function useEventListener<E extends string>(target: EventTarget, event: MaybeArray<E>, listener: Listener<EventTarget, Event>, options?: UseEventListenerOptions): () => void
+
+function useEventListener<T extends EventTarget, M extends Record<string, any>, E extends keyof M>(target: T, event: MaybeArray<E>, listener: Listener<T, M[E]>, options?: UseEventListenerOptions): () => void
+
+function useEventListener(target: EventTarget, event: MaybeArray<string>, listener: Listener<EventTarget, Event>, options?: UseEventListenerOptions): () => void {
   if (Array.isArray(event)) {
     event.forEach((e) => {
       target.addEventListener(e, listener, options)
