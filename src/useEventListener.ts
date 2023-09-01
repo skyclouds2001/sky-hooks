017deb522f5084cdb6b9773c5f5428adbb476e7d
skyclouds2001 <95597335+skyclouds2001@users.noreply.github.com>
2023-09-01 13:10:16 +0800

    Update useEventListener.ts

--- src/useEventListener.ts
+++ src/useEventListener.ts
@@ -1,33 +1,41 @@
-import { tryOnScopeDispose } from '.'
+import { getCurrentScope, onScopeDispose } from 'vue'
 
 type UseEventListenerOptions = AddEventListenerOptions | EventListenerOptions | boolean
 
-function useEventListener<E extends keyof WindowEventMap>(target: Window, event: E, listener: (this: Window, e: WindowEventMap[E]) => void, options?: UseEventListenerOptions): () => void
+function useEventListener<E extends keyof WindowEventMap>(target: Window, event: E | E[], listener: (this: Window, e: WindowEventMap[E]) => void, options?: UseEventListenerOptions): () => void
 
-function useEventListener<E extends keyof DocumentEventMap>(target: Document, event: E, listener: (this: Document, e: DocumentEventMap[E]) => void, options?: UseEventListenerOptions): () => void
+function useEventListener<E extends keyof DocumentEventMap>(target: Document, event: E | E[], listener: (this: Document, e: DocumentEventMap[E]) => void, options?: UseEventListenerOptions): () => void
 
-function useEventListener<E extends keyof ShadowRootEventMap>(target: ShadowRoot, event: E, listener: (this: ShadowRoot, e: ShadowRootEventMap[E]) => void, options?: UseEventListenerOptions): () => void
+function useEventListener<E extends keyof ShadowRootEventMap>(target: ShadowRoot, event: E | E[], listener: (this: ShadowRoot, e: ShadowRootEventMap[E]) => void, options?: UseEventListenerOptions): () => void
 
-function useEventListener<E extends string>(target: EventTarget, event: E, listener: (this: EventTarget, e: Event) => void, options?: UseEventListenerOptions): () => void
+function useEventListener<E extends string>(target: EventTarget, event: E | E[], listener: (this: EventTarget, e: Event) => void, options?: UseEventListenerOptions): () => void
 
-function useEventListener<T extends EventTarget, M extends Record<string, any>, E extends keyof M>(target: T, event: E, listener: (this: T, e: M[E]) => void, options?: UseEventListenerOptions): () => void
+function useEventListener<T extends EventTarget, M extends Record<string, any>, E extends keyof M>(target: T, event: E | E[], listener: (this: T, e: M[E]) => void, options?: UseEventListenerOptions): () => void
 
-/**
- * auto bind event listener when mounted and unbind when unmounted or disposed or if controlled
- * @param target event target
- * @param event event name
- * @param listener event listener callback
- * @param options event listener options
- * @returns cancel event listener
- */
-function useEventListener(target: EventTarget, event: string, listener: (this: EventTarget, e: Event) => void, options?: UseEventListenerOptions): () => void {
-  target.addEventListener(event, listener, options)
-
-  const stop = (): void => {
-    target.removeEventListener(event, listener, options)
+function useEventListener(target: EventTarget, event: string | string[], listener: (this: EventTarget, e: Event) => void, options?: UseEventListenerOptions): () => void {
+  if (Array.isArray(event)) {
+    event.forEach((e) => {
+      target.addEventListener(e, listener, options)
+    })
+  } else {
+    target.addEventListener(event, listener, options)
   }
 
-  tryOnScopeDispose(stop)
+  const stop = (): void => {
+    if (Array.isArray(event)) {
+      event.forEach((e) => {
+        target.removeEventListener(e, listener, options)
+      })
+    } else {
+      target.removeEventListener(event, listener, options)
+    }
+  }
+
+  if (getCurrentScope() !== undefined) {
+    onScopeDispose(() => {
+      stop()
+    })
+  }
 
   return stop
 }
