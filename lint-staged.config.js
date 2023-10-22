0e496497c4eecd111c5d1eb600ce2ffac23ce711
skyclouds2001 <skyclouds2001@163.com>
2023-10-22 15:06:56 +0800

    build: update project config

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,4 @@
 module.exports = {
-  '*.{ts,tsx,vue}': ['eslint --fix'],
-  '*.{ts,tsx,json,vue,html,md,css}': ['prettier --write'],
-  '*.{vue,css}': ['stylelint --fix'],
+  '*.{js,jsx,ts,tsx}': ['eslint --fix'],
+  '*.{js,jsx,ts,tsx,json,md}': ['prettier --write'],
 }
