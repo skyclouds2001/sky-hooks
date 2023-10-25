58b1c8dbbf6baa0585af572a90cd36457a4d84cf
skyclouds2001 <skyclouds2001@163.com>
2023-10-25 13:37:15 +0800

    build: update config

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,13 +3,17 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     root: '.',
+    include: ['**/?(*.){test,spec}.?(c|m)[jt]s?(x)'],
+    exclude: ['**/node_modules/**', '**/dist/**', '**/.{idea,fleet,vscode,git}/**', '**/{vite,vitest}.config.*'],
     watch: false,
     environment: 'jsdom',
-    include: ['**/*.{test,spec}.{js,jsx,ts,tsx}'],
     reporters: ['default', 'html'],
     coverage: {
       provider: 'v8',
       enabled: true,
+    },
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
     },
     cache: {},
   },
