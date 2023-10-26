3840ac51791f47a20b51d4c4637b546a5e46b330
skyclouds2001 <skyclouds2001@163.com>
2023-10-26 12:37:00 +0800

    refactor: update web api - V

--- src/util.ts
+++ src/util.ts
@@ -1,3 +1,9 @@
+/**
+ * basic function type replace of `Function`
+ */
 export type Fn = (...args: any[]) => any
 
+/**
+ * basic object type replace of `object` or `Object`
+ */
 export type Obj = Record<string, any>
