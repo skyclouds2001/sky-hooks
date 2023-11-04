89b36bd0a30a14434df146d4676519ecf9c0664f
skyclouds2001 <skyclouds2001@163.com>
2023-11-04 22:22:32 +0800

    feat: small update

--- src/reactifyFunction.ts
+++ src/reactifyFunction.ts
@@ -1,10 +1,10 @@
-import { computed, unref, type ComputedRef, type Ref } from 'vue'
+import { computed, toValue, type ComputedRef, type MaybeRefOrGetter } from 'vue'
 import { type Fn } from './util'
 
-type Reactify<T> = T extends (...args: infer A) => infer R
+type Reactify<T extends Fn> = T extends (...args: infer A) => infer R
   ? (
       ...args: {
-        [K in keyof A]: A[K] | Ref<A[K]>
+        [K in keyof A]: MaybeRefOrGetter<A[K]>
       }
     ) => ComputedRef<R>
   : never
@@ -19,10 +19,10 @@ const reactifyFunction = function <T extends Fn>(fn: T): Reactify<T> {
     return computed(() =>
       fn.apply(
         this,
-        args.map((v) => unref(v))
+        args.map((v) => toValue(v))
       )
     )
-  } as unknown as Reactify<T>
+  } as Reactify<T>
 }
 
 export default reactifyFunction
