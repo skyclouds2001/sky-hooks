89924dc62e811b89e3a50f2cf22d7d092296b0ca
skyclouds2001 <skyclouds2001@163.com>
2023-11-05 23:11:11 +0800

    build: support https  coi

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -1,5 +1,6 @@
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import fs from 'node:fs'
 import path from 'node:path'
 import { defineConfig } from 'vite'
 
@@ -12,5 +13,19 @@ export default defineConfig({
   plugins: [vue(), vueJsx()],
   css: {
     devSourcemap: true,
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    strictPort: true,
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, './../../../frontend/serve/localhost+1-key.pem')),
+      cert: fs.readFileSync(path.resolve(__dirname, './../../../frontend/serve/localhost+1.pem')),
+    },
+    open: true,
+    headers: {
+      'Cross-Origin-Opener-Policy': 'same-origin',
+      'Cross-Origin-Embedder-Policy': 'require-corp',
+    },
   },
 })
