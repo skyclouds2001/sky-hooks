5efda9fa0ad1fa54f5b38cfc4d8ce82cf1c384a4
skyclouds2001 <skyclouds2001@163.com>
2023-11-06 12:48:21 +0800

    test: useDocumentPictureInPicture

--- playground/App.vue
+++ playground/App.vue
@@ -1,7 +1,23 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useDocumentPictureInPicture } from '@';
+import { ref } from 'vue';
+
+const dpip = ref<HTMLDivElement | null>(null)
+
+const udipi = useDocumentPictureInPicture(dpip)
+</script>
 
 <template>
   <div>sky-hooks</div>
+
+  <div id="document-picture-in-picture">
+    <div ref="dpip">document-picture-in-picture</div>
+    <div class="support">{{ udipi.isSupported }}</div>
+    <div class="status">{{ udipi.isDocumentPictureInPicture }}</div>
+    <button class="enter" @click="udipi.enter()">enter</button>
+    <button class="exit" @click="udipi.exit()">exit</button>
+    <button class="toggle" @click="udipi.toggle()">toggle</button>
+  </div>
 </template>
 
 <style scoped lang="scss"></style>
