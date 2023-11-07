aa0c650edcb6dda15b23b926913387c2b6f48fd9
skyclouds2001 <skyclouds2001@163.com>
2023-11-07 12:06:49 +0800

    test: add test for usePictureInPicture

--- playground/App.vue
+++ playground/App.vue
@@ -1,10 +1,14 @@
 <script setup lang="ts">
-import { useDocumentPictureInPicture } from '@';
-import { ref } from 'vue';
+import { useDocumentPictureInPicture, usePictureInPicture } from '@'
+import { ref } from 'vue'
 
 const dpip = ref<HTMLDivElement | null>(null)
 
-const udipi = useDocumentPictureInPicture(dpip)
+const udpip = useDocumentPictureInPicture(dpip)
+
+const pip = ref<HTMLVideoElement | null>(null)
+
+const upip = usePictureInPicture(pip)
 </script>
 
 <template>
@@ -12,11 +16,20 @@ const udipi = useDocumentPictureInPicture(dpip)
 
   <div id="document-picture-in-picture">
     <div ref="dpip">document-picture-in-picture</div>
-    <div class="support">{{ udipi.isSupported }}</div>
-    <div class="status">{{ udipi.isDocumentPictureInPicture }}</div>
-    <button class="enter" @click="udipi.enter()">enter</button>
-    <button class="exit" @click="udipi.exit()">exit</button>
-    <button class="toggle" @click="udipi.toggle()">toggle</button>
+    <div class="support">{{ udpip.isSupported }}</div>
+    <div class="status">{{ udpip.isDocumentPictureInPicture }}</div>
+    <button class="enter" @click="udpip.enter()">enter</button>
+    <button class="exit" @click="udpip.exit()">exit</button>
+    <button class="toggle" @click="udpip.toggle()">toggle</button>
+  </div>
+
+  <div id="picture-in-picture">
+    <video ref="pip" class="video" src="/demo.mp4" width="300" height="200"></video>
+    <div class="support">{{ upip.isSupported }}</div>
+    <div class="status">{{ upip.isPictureInPicture }}</div>
+    <button class="enter" @click="upip.enter()">enter</button>
+    <button class="exit" @click="upip.exit()">exit</button>
+    <button class="toggle" @click="upip.toggle()">toggle</button>
   </div>
 </template>
 
